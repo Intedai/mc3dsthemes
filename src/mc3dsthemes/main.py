c77a1e43a1fbdcf71b5f48f6f3e40105f494e100
Intedai <69306633+Intedai@users.noreply.github.com>
2026-04-06 16:39:09 +0300

    moved and created theme related asset creation to a seperate file , generating tmp folder with theme assets that will be used in theme creation

--- src/mc3dsthemes/main.py
+++ src/mc3dsthemes/main.py
@@ -1,28 +1,27 @@
-from .block_functions import render_rectangle_img, get_block_img, average_block_color
-from PIL import Image
-
-# Top screen x,z coords
-top_start_xz = (-8, 6)
-top_end_xz = (-32,-8)
-
-# Bottom screen x,z coords
-bottom_start_xz = (-11,-12)
-bottom_end_xz = (-30, -26)
+import os
+from shutil import rmtree
+from pathlib import Path
+from .theme_data import create_icons, render_minecraft_screens, create_screen_imgs, create_preview_img
 
 # TEMP: Just keeping this for now, will probs change to a config.toml file
-overworld_path = "/home/intedai/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/26.1.1/minecraft/saves/3ds theme maker/dimensions/minecraft/overworld/region/"
+OVERWORLD_PATH = "/home/intedai/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/26.1.1/minecraft/saves/3ds theme maker/dimensions/minecraft/overworld/region/"
 
-# Icon part will be moved from main in the future
-icon_xyz = (-30, -63, -30)
+def main():
+    #author_name = input("Enter author name: ")
+    
+    temp_dir = Path("tmp")
+    temp_dir.mkdir(exist_ok=True)
 
-icon_mc_texture = get_block_img(overworld_path, *icon_xyz)
-icon = icon_mc_texture.resize((48, 48), Image.NEAREST)
-small_icon = icon_mc_texture.resize((24, 24), Image.NEAREST)
+    create_icons(temp_dir, OVERWORLD_PATH)
+    
+    top_img, bottom_img = render_minecraft_screens(OVERWORLD_PATH)
+    create_screen_imgs(top_img, bottom_img, temp_dir)
+    
+    create_preview_img(top_img, bottom_img)
 
-icon.show()
-small_icon.show()
+    if temp_dir.exists():
+        #rmtree(temp_dir)
+        pass
 
-print(f"Red wool average color: {hex(average_block_color("red_wool"))}")
-
-render_rectangle_img(overworld_path, top_start_xz, top_end_xz).show()
-render_rectangle_img(overworld_path, bottom_start_xz, bottom_end_xz).show()
+if __name__ == "__main__":
+    main()
---
+++ src/mc3dsthemes/theme_data.py
@@ -0,0 +1,53 @@
+from .block_functions import render_rectangle_img, get_block_img, average_block_color
+from PIL import Image
+from pathlib import Path
+
+SCREEN_IMG_SIZE = (512, 256)
+
+# Top screen
+TOP_START_X_Z = (-8, 6)
+TOP_END_X_Z = (-32,-8)
+TOP_PASTE_POS = (6, 0)
+TOP_SCREEN_FILE_NAME = "top.png"
+
+# Bottom screen
+BOTTOM_START_X_Z = (-11,-12)
+BOTTOM_END_X_Z = (-30, -26)
+BOTTOM_PASTE_POS = (0, 0)
+BOTTOM_SCREEN_FILE_NAME = "bottom.png"
+
+# Icon
+ICON_X_Y_Z = (-30, -63, -30)
+ICON_SIZE = (48, 48)
+ICON_FILE_NAME = "icon.png"
+SMALL_ICON_SIZE = (24, 24)
+SMALL_ICON_FILE_NAME = "small_icon.png"
+
+def create_icons(output_dir: Path, overworld_path: Path) -> None:
+    icon_mc_texture = get_block_img(overworld_path, *ICON_X_Y_Z)
+    
+    icon = icon_mc_texture.resize(ICON_SIZE, Image.NEAREST)
+    small_icon = icon_mc_texture.resize(SMALL_ICON_SIZE, Image.NEAREST)
+
+    icon.save(output_dir / ICON_FILE_NAME)
+    small_icon.save(output_dir / SMALL_ICON_FILE_NAME)
+
+def render_minecraft_screens(overworld_path: Path) -> tuple[Image.Image, Image.Image]:
+    top_minecraft_render = render_rectangle_img(overworld_path, TOP_START_X_Z, TOP_END_X_Z)
+    bottom_minecraft_render = render_rectangle_img(overworld_path, BOTTOM_START_X_Z, BOTTOM_END_X_Z)
+
+    return top_minecraft_render, bottom_minecraft_render
+
+def create_screen_imgs(top_minecraft_render: Image.Image, bottom_minecraft_render: Image.Image, output_dir: Path) -> None:
+    top_final = Image.new("RGB", SCREEN_IMG_SIZE)
+    bottom_final = Image.new("RGB", SCREEN_IMG_SIZE)
+
+    top_final.paste(top_minecraft_render, TOP_PASTE_POS)
+    bottom_final.paste(bottom_minecraft_render, BOTTOM_PASTE_POS)
+
+    top_final.save(output_dir / TOP_SCREEN_FILE_NAME)
+    bottom_final.save(output_dir / BOTTOM_SCREEN_FILE_NAME)
+
+def create_preview_img(top_minecraft_render: Image.Image, bottom_minecraft_render: Image.Image) -> None:
+    pass
+#print(f"Red wool average color: {hex(average_block_color("red_wool"))}")
